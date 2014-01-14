122b862e3d1660b9ecb9efc0cd40739da3af333f
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-13 23:33:43 -0700

    add GitHub endpoint, and use FooFoBerry Gem to create a new GitHub notification

--- spec/costner/costner_spec.rb
+++ spec/costner/costner_spec.rb
@@ -8,9 +8,18 @@ describe Costner do
     Costner.new
   end
 
+  def content_type
+    {"Content-Type" => "application/json"}
+  end
+
   it "should 200 response for a post on GitHub Callback" do
-    post '/github'
+    json = File.read('./spec/fixtures/github_payload.json')
+    FooFoBerry::GitHubNotification.any_instance.stub(:save!)
+    post '/github',  { "payload" => json }, content_type
+
     expect(last_response.status).to eq 200
+    #expect_any_instance_of(FooFoBerry::GitHubNotification).to receive(:save!)
+    #FooFoBerry::GitHubNotification.any_instance.should_receive(:save!).with()
   end
 
 end
