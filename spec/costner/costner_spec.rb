5407c3388304a5cb56528e9b5fd8403f7a398f90
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-16 09:41:37 -0700

    fix test with Katrina

--- spec/costner/costner_spec.rb
+++ spec/costner/costner_spec.rb
@@ -13,13 +13,31 @@ describe Costner do
   end
 
   it "should 200 response for a post on GitHub Callback" do
-    json = File.read('./spec/fixtures/github_payload.json')
-    FooFoBerry::GitHubNotification.any_instance.stub(:save!)
+    json = "{}"
+    data = double
+    FooFoBerry::GitHubNotification.stub(:new).with(json).and_return(data)
+    expect(data).to receive(:save!)
+    #expect_any_instance_of(FooFoBerry::GitHubNotification).to receive(:save!)
+
     post '/github',  { "payload" => json }, content_type
 
+    print last_response.body
     expect(last_response.status).to eq 200
+
     #expect_any_instance_of(FooFoBerry::GitHubNotification).to receive(:save!)
     #FooFoBerry::GitHubNotification.any_instance.should_receive(:save!).with()
+    #
+    # STUB OUT NEW with params
+    # => to send back stub, and expect that naked stub to receive :save!
+    # magic tricks of testing Sandi Metz
+
+    # 1.
+    # can assert api gives you the right things
+    # 2.
+    # given i've received this, i return this thing
+    #
+    # PROBLEM: What if they drift?
+    # SOLUTION: Sanity check with VCR
   end
 
 end
